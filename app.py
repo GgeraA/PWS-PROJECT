00db00b71996c43e88f23945fb126e834afe3754
GgeraA <142055522+GgeraA@users.noreply.github.com>
2025-09-24 14:23:17 -0600

    Configuración del entorno de SMTP

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from routes.main import main_bp
 from routes.auth import auth_bp
-from routes.users import users_bp  # si tienes uno para usuarios
+from routes.users import users_bp
+from config import Config
+from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = "supersecret"  # cámbialo por uno seguro
+    app.config.from_object(Config)  # Carga config desde config.py
+    
+        # Swagger UI
+    SWAGGER_URL = "/docs"
+    API_URL = "/static/swagger.yaml"
+    swaggerui_blueprint = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={"app_name": "PWS Project API"}
+    )
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
     # Registrar Blueprints
     app.register_blueprint(main_bp, url_prefix="/")
--- config.py
+++ config.py
@@ -1,8 +1,19 @@
-# config.py
-DATABASE = {
-    "dbname": "seguridad",
-    "user": "postgres",      
-    "password": "ggerahl08ROM#",      
-    "host": "localhost",
-    "port": 5432
-}
+import os
+from dotenv import load_dotenv
+
+DATABASE = { "dbname": "seguridad", "user": "postgres", "password": "ggerahl08ROM#", "host": "localhost", "port": 5432 }
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
+    
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    # Config correo
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
+    MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
